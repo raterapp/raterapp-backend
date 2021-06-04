da2aecc8cce2e22023384af999afc85302d75427
Viacheslav Shvets <viacheslav_shvets@external.mckinsey.com>
2021-06-04 15:49:52 +0300

    vypizd hello world; added swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UsersModule, RatingsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+function setupValidationPipe(app: INestApplication) {
+  app.useGlobalPipes(new ValidationPipe());
+}
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Rater app API')
+    .setDescription('API for rater app')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  setupValidationPipe(app);
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
